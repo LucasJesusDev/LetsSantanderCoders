ab8a08c3a81221c278a739f43265910f851fa54b
LucasJesusDev <lucas.958@hotmail.com>
2023-05-28 23:09:23 -0300

    CompleteAcoes

--- POO_Animais/src/Cachorro.java
+++ POO_Animais/src/Cachorro.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Cachorro {
+public class Cachorro {
+
+    public String nome;
+    public String cor;
+    public int altura;
+    public double peso;
+    public int tamanhoDoRabo;
+
+    public String estadoDeEspirito;
+
+    public void comer() {
+        System.out.println("Comendo...");
+    }
+
+    public void latir() {
+        System.out.println("Auuu");
+    }
+
+    public String pegar() {
+        return "Bolinha";
+    }
+
+    public String interagir(String acao) {
+        if (acao.equals("carinho")) {
+            this.estadoDeEspirito = "feliz :-)";
+        } else if (acao.equals("vai dormir !")) {
+            this.estadoDeEspirito = "Bravo :-(";
+        } else {
+            this.estadoDeEspirito = "Neutro :-|";
+        }
+        return estadoDeEspirito;
+
+    }
 }
+
