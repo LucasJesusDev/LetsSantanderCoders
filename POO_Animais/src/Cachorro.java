a69a97457a9271ac7a50437ff1ca486a8d60de43
LucasJesusDev <lucas.958@hotmail.com>
2023-05-28 23:53:08 -0300

    Contrutor

--- POO_Animais/src/Cachorro.java
+++ POO_Animais/src/Cachorro.java
@@ -1,12 +1,74 @@
 public class Cachorro {
 
-    public String nome;
-    public String cor;
-    public int altura;
-    public double peso;
-    public int tamanhoDoRabo;
+    private String nome;
+    private String cor;
+    private int altura;
+    private double peso;
+    private int tamanhoDoRabo;
+    private String estadoDeEspirito;
 
-    public String estadoDeEspirito;
+    //construtores
+
+    public Cachorro() {
+    }
+
+    public Cachorro(String nome, String cor, int altura, double peso, int tamanhoDoRabo, String estadoDeEspirito) {
+        this.nome = nome;
+        this.cor = cor;
+        this.altura = altura;
+        this.peso = peso;
+        this.tamanhoDoRabo = tamanhoDoRabo;
+        this.estadoDeEspirito = estadoDeEspirito;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getTamanhoDoRabo() {
+        return tamanhoDoRabo;
+    }
+
+    public void setTamanhoDoRabo(int tamanhoDoRabo) {
+        this.tamanhoDoRabo = tamanhoDoRabo;
+    }
+
+    public String getEstadoDeEspirito() {
+        return estadoDeEspirito;
+    }
+
+    public void setEstadoDeEspirito(String estadoDeEspirito) {
+        this.estadoDeEspirito = estadoDeEspirito;
+    }
+
 
     public void comer() {
         System.out.println("Comendo...");
@@ -21,14 +83,32 @@ public class Cachorro {
     }
 
     public String interagir(String acao) {
-        if (acao.equals("carinho")) {
-            this.estadoDeEspirito = "feliz :-)";
-        } else if (acao.equals("vai dormir !")) {
-            this.estadoDeEspirito = "Bravo :-(";
-        } else {
-            this.estadoDeEspirito = "Neutro :-|";
+
+        switch (acao) {
+            case "carinho":
+                this.estadoDeEspirito = "feliz";
+                break;
+            case "vai dormir !":
+                this.estadoDeEspirito = "Bravo";
+                break;
+            case "pisar na patinha":
+                this.estadoDeEspirito = "triste";
+                break;
+            default:
+                this.estadoDeEspirito = "neutro";
+                break;
         }
-        return estadoDeEspirito;
+        return this.estadoDeEspirito;
+
+
+//        if (acao.equals("carinho")) {
+//            this.estadoDeEspirito = "feliz :-)";
+//        } else if (acao.equals("vai dormir !")) {
+//            this.estadoDeEspirito = "Bravo :-(";
+//        } else {
+//            this.estadoDeEspirito = "Neutro :-|";
+//        }
+//        return estadoDeEspirito;
 
     }
 }
--- POO_Animais/src/Main.java
+++ POO_Animais/src/Main.java
@@ -2,12 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Cachorro cachorro1 = new Cachorro();
-        cachorro1.nome = "Laka";
-        cachorro1.cor = "Cinza";
-        cachorro1.altura = 25;
-        cachorro1.peso = 5.5;
-        cachorro1.tamanhoDoRabo = 5;
+        Cachorro cachorro1 = new Cachorro("Laka", "Cinza", 25, 5.5, 5, "nada");
+        Cachorro cachorro2 = new Cachorro();
 
         cachorro1.latir();
         cachorro1.comer();
